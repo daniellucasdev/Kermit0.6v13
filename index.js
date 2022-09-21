6c35fb80b5a6d8e03d7484f2ba1c8301d15dc17b
Daniel Lucas Ferreira <daniel.ferreira2675@gmail.com>
2022-09-21 11:33:35 -0300

    1.1

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js')
 require('dotenv').config()
 
-const TOKEN = "MTAyMTYwMDY3OTU2MDc2MTQ0NQ.G8H-lP.nP1IndjmlV9-1qdaIPmRql0zZL3ayPcxGv3x0c"
-
 const client = new Discord.Client({
     intents: [
         'GUILDS',
