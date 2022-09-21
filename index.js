8137f13deee6136a8ed8577f2d1e1ea23c48c0cb
Daniel Lucas Ferreira <daniel.ferreira2675@gmail.com>
2022-09-21 12:19:09 -0300

    img invite on

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require('discord.js')
 require('dotenv').config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
         'GUILDS',
-        'GUILD_MESSAGES'
+        'GUILD_MESSAGES',
+        'GUILD_MEMBERS'
     ]
 })
 
@@ -16,6 +19,16 @@ client.on("messageCreate", (message) =>{
     if (message.content == 'hi') {
         message.reply('Salve')
     }
+})
+
+const welcomeChannelId = '1013982982970802216'
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Bem vindo á essa bosta`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
